9f064b214f45fc61d90b17e294cb1a4c7e66b280
Alexandre-Georges <alexandre.georges.ra@gmail.com>
2014-01-27 16:26:53 -0800

    Converter with functions

--- web/converter.dart
+++ web/converter.dart
@@ -1,14 +1,17 @@
 library converter;
 
-class Converter {
-  static const double LITRES_IN_ONE_GALLON = 1.0;
-  static const double KILOMETERS_IN_ONE_MILE = 1.0;
+  const double LITRES_IN_ONE_GALLON = 1.0;
+  const double METERS_IN_ONE_MILE = 1600.0;
   
-  static double MPG_TO_L100KM(mpg) {
-    return KILOMETERS_IN_ONE_MILE;
+  double NO_CONVERSION(value) => value;
+
+  double MILES_TO_METERS(value) => METERS_IN_ONE_MILE * value;
+  double KILOMETERS_TO_METERS(value) => 1000.0 * value;
+  
+  double MPG_TO_L100KM(mpg) {
+    return METERS_IN_ONE_MILE;
   }
   
-  static double L100KM_TO_MPG(l100Km) {
+  double L100KM_TO_MPG(l100Km) {
     return LITRES_IN_ONE_GALLON;
   }
-}
