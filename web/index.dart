4db63c16b925fccf568d8387f4fb8b93469371c5
Alexandre-Georges <alexandre.georges.ra@gmail.com>
2014-01-30 16:04:43 -0800

    Styling

--- web/index.dart
+++ web/index.dart
@@ -3,12 +3,12 @@ import 'converter.dart';
 import 'Unit.dart';
 
 void  main() {
-
+/*
   Unit metre = new Unit('metre', NO_CONVERSION);
   Unit kilometre = new Unit('kilometre', KILOMETERS_TO_METERS);
   Unit mile = new Unit('mile', MILES_TO_METERS);
   print(mile.convertToReferenceUnit(10.0));
-  print(mile.convertTo(2.0, kilometre));
+  print(mile.convertTo(2.0, kilometre));*/
   /*
   InputElement inputField = querySelector('#input-mpg');
   inputField.onInput.listen(mpgEvent);*/
--- web/model.dart
+++ web/model.dart
@@ -1,3 +1,3 @@
 class Model {
-  
+  List<Unit>
 }
