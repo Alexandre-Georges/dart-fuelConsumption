9a0461684e9913ebc440b77aaf725f51dbaa3c1e
Alexandre-Georges <alexandre.georges.ra@gmail.com>
2014-02-23 19:08:51 -0800

    Using a double

--- web/model/reference_unit.dart
+++ web/model/reference_unit.dart
@@ -3,6 +3,6 @@ import '../converter/converter.dart';
 
 class ReferenceUnit extends Unit {
   ReferenceUnit(String name) : super(name, IDENTITY, IDENTITY) {
-    this.value = 0;
+    this.value = 0.0;
   }
 }
