63993832f40b7123b48490e408dc198de2d0821b
Alexandre-Georges <alexandre.georges.ra@gmail.com>
2014-03-06 14:22:44 -0800

    Adding observers

--- web/polymer/my_model_element.dart
+++ web/polymer/my_model_element.dart
@@ -2,6 +2,7 @@ import 'package:polymer/polymer.dart';
 
 import '../model/model.dart';
 import 'main_element.dart';
+import '../observers/object_observer.dart';
 import '../observers/list_object_observer.dart';
 
 @CustomTag('my-model')
@@ -18,6 +19,11 @@ class MyModelElement extends MainElement {
     ListObjectObserver<Unit, double> observer = new ListObjectObserver<Unit, double>(this.model.units, 'value');
     observer.changes.listen((List<ObjectChangeRecord> changeRecords) {
       print('here ' + changeRecords[0].object.toString() + ' ' + changeRecords[0].object.name + ' ' + changeRecords[0].oldValue.toString() + ' ' + changeRecords[0].newValue.toString());
+    });
+
+    ObjectObserver<Unit, double> referenceObserver = new ObjectObserver<Unit, double>(this.model.referenceUnit, 'value');
+    referenceObserver.changes.listen((List<ObjectChangeRecord> changeRecords) {
+      print('here referenceObserver ' + changeRecords[0].object.toString() + ' ' + changeRecords[0].object.name + ' ' + changeRecords[0].oldValue.toString() + ' ' + changeRecords[0].newValue.toString());
     });
   }
 }
