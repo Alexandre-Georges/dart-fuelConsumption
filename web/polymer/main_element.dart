327a8725f013acd516c2eded67c77f898fd0a2e0
Alexandre-Georges <alexandre.georges.ra@gmail.com>
2014-08-07 15:38:57 -0700

    Unused code

--- web/polymer/main_element.dart
+++ web/polymer/main_element.dart
@@ -10,9 +10,6 @@ class MainElement extends PolymerElement {
   MainElement.created() : super.created();
 
   @override
-  bool get applyAuthorStyles => true;
-
-  @override
   BindingDelegate syntax = MyPolymerExpressions.INSTANCE;
 
 }
